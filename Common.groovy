5649c6be2c88c5ec9d0c5090ab6fa048d4aaa7d0
pinnamraju mallikarjunarao <malli2016@outlook.com>
2017-12-29 17:10:17 +0530

    Updated Common.groovy
    Hi this is Mallikarjun from qentelli i have updated Common.groovy file sent you pull request can you please check.

--- Common.groovy
+++ Common.groovy
@@ -1,4 +1,4 @@
-def prep_common(def build_env){
+/*def prep_common(def build_env){
   println("will deploy to ${build_env}")
   def name = sh (script: 'git whatchanged -n 1 --pretty=format: --name-only',returnStdout:true).trim().split('/')
   for (i = 0; i < name.length; i++){
@@ -30,6 +30,72 @@ def prep_common(def build_env){
  }
 }
 return this
+*/
+setup() 
 
+{
+
+
+
+}
+def start() {
+
+	if (service == ui)
+	{
+	@Library('PSL') _
+	def common_jenkins = new ors.utils.common_jenkins(steps, env)
+	def common_scm = new ors.utils.common_scm(steps, env)
+
+	 //   environment {
+			/*
+			These variables should change according to team/project
+			*/
+			def TEAM_SLACK_CHANNEL="#spg-feynman-ci"
+			def TEAM_EMAIL="m2s6m0x4t8p6v0q0@autodesk.slack.com"
+			def SERVICE_ACCOUNT=credentials('svc_p_account_id')
+			def VAULT_PATH = 'spg/feynman-dev/aws/adsk-eis-feynman-dev/sts/admin'
+			def CDN="s3://cdn-dev-manage.autodesk.com"
+			/*
+			These shouldn't change unless you have your own build container or NPM registry
+			*/
+			def NPM_REGISTRY="https://art-bobcat.autodesk.com/artifactory/"
+			def NPM_AUTH="api/npm/auth"
+			def NPM_VIRTUAL_REPO="api/npm/autodesk-npm-virtual/"
+			def BUILD_CONTAINER_IMAGE="autodesk-docker.art-bobcat.autodesk.com:10873/team-account/build-npm:latest"
+			def DEPLOY_CONTAINER_IMAGE="autodesk-docker.art-bobcat.autodesk.com:10873/team-spg/bedrock-build-terraform:latest"
+			def VAULT_ADDR = 'https://vault.aws.autodesk.com'
+			def BUILD_INFO="${env.JOB_NAME} ${env.BUILD_NUMBER} \n ${env.BUILD_URL}";
+			def SLACK_TOKEN=credentials('ors_slack_token')
+			def CI=true
+	  //  }
+	}
+//lambda	
+	
+	if (service == lambda){	
+	//def WORKER = 'cloud&&centos'
+
+	// Build Configuration
+	def BUILD_IMAGE = 'node:8'
+	def BUILD_INFO_LAMBDA = "${env.JOB_NAME}-${env.BUILD_NUMBER}\n${env.BUILD_URL}"
+
+	// Deploy Configuration
+	def DEPLOY_TOOLS_IMAGE = 'autodesk-docker.art-bobcat.autodesk.com:10873/team-spg/bedrock-build-terraform:latest'
+
+	// Notifications
+	def TEAM_SLACK_CHANNEL = '#spg-feynman-ci'
+
+	// Build Status
+	def SUCCESS = 'SUCCESS'
+	def isMasterBranch = false
+
+	}
+
+	pipeline {
+		agent {
+			label 'cloud&&centos'
+		}
+	 }
+ 
+ }
 
 
